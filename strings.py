7e6f88f1edb027eeedce6690baf751ca8c5992fb
Sugarkhuu <sugarkhuul@gmail.com>
2018-12-09 14:01:21 +0100

    Before recording

--- strings.py
+++ strings.py
@@ -4,48 +4,70 @@ Created on Tue Nov 27 23:15:19 2018
 
 @author: Sugar2
 """
-# Reference
+
 # https://www.tutorialspoint.com/python/python_strings.htm
 
+# "" and '' are the same
 
-str1 = "Hello World!"
-str2 = 'Hi Mongol!'
+my_str = "Hello World!"
+str1 = ''
 
-str1.replace('o','y')
-str2.replace('l', 'lians')
+#Basics
 
-str2[4]
-str2[0]
-str2[0:1]
-str2[0:5]
-str2[2:5]
-str2[-1]
-str2[-2]
-str2[0:-2]
-str2[20]
-str2[-7]
-str2[-10]
-str2[-11]
-str2[9]
+my_str.replace('o','y')
 
-#Attach
-str3 = str1 + str2
-print(str3)
-str3 = str1 + " " + str2
+my_str[4]
+my_str[0]
+my_str[0:1]
+my_str[0:5]
+my_str[2:5]
+my_str[-1]
+my_str[-2]
+my_str[0:-2]
+my_str[20]
+my_str[-7]
+my_str[-10]
+my_str[-11]
+my_str[9]
+
+#Legnth and others
+
+len(my_str)
+max(my_str)
+min(my_str)
+
+# Strip
+
+str = "0000000wow l daa!!!0000000";
+print(str.strip( '0' ))
+
+# Up down
+
+my_str.upper()
+my_str.lower()
+my_str.capitalize()
+my_str.title()
+
 
 #Repeat
-str2 * 5
+my_str * 5
 
 # Reverse
-str2[1:5:2]
-str2[::-1]
+#my_str[1:5:2]
+my_str[::-1]
 
-#Membership not in
-str2 in (str2, str1)
+#Membership in or not in
+
+my_str in (my_str, 'Chiher', 'Jims')
+
+my_word = 'World'
+
+my_str.find(my_word,0)
+my_str.index(my_word,0)
 
 
 # string format operator %
-# make 2 in string
+
 dog_1 = 'Асар'
 dog_2 = 'Басар'
 event = "амьдран суудаг байжээ."
@@ -53,39 +75,29 @@ event = "амьдран суудаг байжээ."
 print("%s, %s %d %s" %(dog_1, dog_2, 2, event))
 
 
-pi = 22/7
-
-print("Пи тоо нь ойролцоогоор " + str(pi) + "-тай тэнцүү.")
-
-
-len(str2)
-max(str2)
-min(str1)
-
-Hi = 'Hi'
-
-str2.find(Hi,0)
-str2.index(Hi,0)
-
-seq = ("Бат", "Эрдэнэ")
-s = "::"
-s.join(seq)
+# Split
 
 str = "Бат-Эрдэнэ Баяр-Эрдэнэ Оюун-Эрдэнэ";
 str.split()
 str.split(' ')
 str.split("-")
 
-str = "0000000wow l daa!!!0000000";
-print(str.strip( '0' ))
 
-str.upper()
-str.lower()
+#Join
 
-str = "tesla was very smart"
-str.capitalize()
-str.title()
+str3 = str1 + my_str
+print(str3)
+str3 = str1 + " " + my_str
 
+seq = ("Бат", "Эрдэнэ")
+s = "::"
+s.join(seq)
+
+
+#Conversion to string
+
+pi = 22/7
+print("Пи тоо нь ойролцоогоор " + str(pi) + "-тай тэнцүү.")
 
 
 
